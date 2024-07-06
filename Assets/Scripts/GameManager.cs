65f0c0695d748d52a3fd87e5c815d226e5dc5ce4
akinmackensen <akinmackensen@gmail.com>
2024-07-06 15:20:24 +0300

    objeekle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int healt = 3;
+
+   
+    void Update()
+    {
+        Debug.Log(healt);
+        
+    }
+}
